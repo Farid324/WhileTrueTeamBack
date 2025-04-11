b03aef36ee44915ba76009396f7a94d7e590e851
Farid <geelgaga@gmail.com>
2025-04-11 17:41:41 -0400

    Validar inicio de sesión exitoso con credenciales correctas

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+const prisma = new PrismaClient();
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await prisma.usuario.findUnique({
+        where: { email },
+      });
+  
+      if (!user) {
+        return res.status(401).json({ message: 'Usuario no encontrado' });
+      }
+  
+      if (user.contraseña !== password) {
+        return res.status(401).json({ message: 'Contraseña incorrecta' });
+      }
+  
+      return res.json({ message: 'Login exitoso', user: { email: user.email } });
+  
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Error en el servidor' });
+    }
+  };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.routes';
 // Cargar variables de entorno
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', authRoutes);
 
 // End point para verificar la salud de la conexión de la API
 app.get('/health', (req, res) => {
