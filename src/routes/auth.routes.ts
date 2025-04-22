d19e2e45a8481687b27c2da305de2468b1870dce
Domis382 <naviaeddy@gmail.com>
2025-04-22 10:51:55 -0400

    se añadieron varias capas de autenticacion para registrarse con google, en los porximos commits se eliminaran para posteriormente ser subidos por las personas correspontes a su trabajo

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,55 @@
 // src/routes/auth.routes.ts
-import { Router } from 'express';
-import { register, login } from '@/controllers/auth.controller'; // 👈 IMPORTA BIEN AQUÍ
-import { validateRegister } from '@/middlewares/validateRegister'; // 👈 IMPORTAR middleware de validación
-import { validateLogin } from '@/middlewares/validateLogin'; 
+import { Router } from "express";
+import { register, login } from "@/controllers/auth.controller"; // 👈 IMPORTA BIEN AQUÍ
+import { validateRegister } from "@/middlewares/validateRegister"; // 👈 IMPORTAR middleware de validación
+import { validateLogin } from "@/middlewares/validateLogin";
+import passport from "passport";
+import { updateGoogleProfile } from "../controllers/auth.controller";
+/* import { isAuthenticated } from "@/middlewares/isAuthenticated"; */
+
 
 const router = Router();
 
-router.post('/register', validateRegister, register);
-router.post('/login', validateLogin, login);
+/* router.patch("/update-profile", updateGoogleProfile); */
 
+router.post("/google/complete-profile", updateGoogleProfile);
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+      failureRedirect: 'http://localhost:3000?error=google',
+      session: true,
+    }),
+    (req, res) => {
+      // 🔥 Redirige al front para que abra el modal de completar perfil
+      res.redirect("http://localhost:3000/home?googleComplete=true");
+    }
+  );
+router.get("/auth/success", (req, res) => {
+  res.send("Inicio de sesión con Google exitoso!");
+});
+
+router.patch("/update-profile", updateGoogleProfile);
+
+router.get("/auth/failure", (req, res) => {
+  res.send("Fallo al iniciar sesión con Google.");
+});
+
+router.post("/register", validateRegister, register);
+router.post("/login", validateLogin, login);
+
+passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000/home?error=cuentaExistente",
+    session: true,
+  }),
+  (req, res) => {
+    res.redirect("http://localhost:3000/home?googleComplete=true");
+  }
+  
 
 export default router;
